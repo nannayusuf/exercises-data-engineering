7ea3a0c1a4c209d3958180fa48a6d93b7d7247b6
nannayusuf <nannayusuf@gmail.com>
2025-03-15 17:34:53 -0300

    added ThreadPoolExecutor for asyn donwloads

--- main.py
+++ main.py
@@ -45,3 +45,31 @@ def main():
 
 if __name__ == "__main__":
     main()
+import aiohttp
+import asyncio
+import os
+
+download_uris = [
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
+]
+
+def create_downloads_dir():
+    download_dir = "downloads"
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
+    return download_dir
+
+async def download_file(session, url, download_dir):
+    filename = url.split("/")[-1]
+    file_path = os.path.join(download_dir, filename)
+    
+    async with session.get(url) as response:
+        if response.status == 200:
+            with open(file_path, 'wb') as file:
+                file.write(await response.read())
+            
